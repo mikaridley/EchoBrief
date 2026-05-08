6e67050c68e13c0fd14a2b5c9f81c71ffe1a60b3
Mika Ridley <mikaridley26@gmail.com>
2026-05-08 16:49:22 +0300

    add dexcription to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,11 @@ export function Home() {
   return (
     <section className="home-page">
       <Hero />
+
+      <section className="home-page__description">
+        <h2>Stop worrying about missing a meeting.<br /> Upload your meeting recording and let our AI extract the bottom line for you!</h2>
+      </section>
+      
     </section>
   )
 }
