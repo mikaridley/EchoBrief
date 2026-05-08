e61f7350741da708b9783a5711a3277284359f27
Mika Ridley <mikaridley26@gmail.com>
2026-05-08 16:24:46 +0300

    set home cmp setup

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,6 @@
 export function Home() {
   return (
-    <>
-      <h1 className="app__title">Home</h1>
-      <p className="app__subtitle">Your home page content goes here.</p>
-    </>
-  )
+    <section className="home-page">
+    </section>
+  );
 }
-
