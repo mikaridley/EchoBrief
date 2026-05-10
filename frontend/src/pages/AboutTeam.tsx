f4eebe975119a988fd68b5c3cfc2cca120c04a83
Mika Ridley <mikaridley26@gmail.com>
2026-05-10 17:24:27 +0300

    add about me

--- frontend/src/pages/AboutTeam.tsx
+++ frontend/src/pages/AboutTeam.tsx
@@ -1,8 +1,16 @@
+import mikaImg from '../assets/imgs/mika-img.jpeg'
+
 export function AboutTeam() {
   return (
-    <>
-      <h1 className="app__title">About the team</h1>
-      <p className="app__subtitle">Introduce the team here.</p>
-    </>
+    <section className="about-team">
+      <h1 className="app__title">Hi, nice to meet ya!</h1>
+      <h2 className="app__subtitle">I'm Mika Ridley</h2>
+
+      <img className="about-team__img" src={mikaImg} alt="Mika" />
+
+      <p>I am a Full Stack Developer who loves building with code and AI. I like to think three steps ahead to ensure the technology aligns with business goals, while always striving to learn something new. I’m a fast learner who stays curious, works hard, and keeps things simple. I’m also big on clear communication and just want to build great stuff with good people.</p>
+
+      <h2>Let's Work Together!</h2>
+    </section>
   )
 }
